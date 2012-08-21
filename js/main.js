5c9115b4f49c8e647597167fd2c57403b1d39c7f
Chris Stromquist <chris.stromquist@gmail.com>
2012-08-20 22:57:15 -0500

    Fixing URL issue...it's too late.

--- js/main.js
+++ js/main.js
@@ -19,8 +19,8 @@
 var Config = {
 	uuid: function(a,b){for(b=a='';a++<36;b+=a*51&52?(a^15?8^Math.random()*(a^20?16:4):4).toString(16):'-');return b},
 	root_url: document.domain,
-	subdirectory: '',
-	sub_url: '/?chapter=',
+	subdirectory: '/',
+	sub_url: '?chapter=',
 	getUrl: function() {
 		return this.root_url + this.subdirectory;
 	}
