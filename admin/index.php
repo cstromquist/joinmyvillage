3998e3c3829e76dcdd94082a06b6b43f0baf1d42
Chris Stromquist <chris.stromquist@gmail.com>
2012-08-22 18:16:09 -0500

    Fixing admin index

--- admin/index.php
+++ admin/index.php
@@ -1,7 +1,7 @@
 <?php
 
 $domain = $_SERVER['SERVER_NAME'] . "/";
-$subdir = "";
+$subdir = "storyofmaya/";
 $url = $domain . $subdir;
 
 $data = array(
@@ -34,7 +34,7 @@ for($i=0; $i<6; $i++) {
 	$chapter = $i+1;
 	$u = $url;
 	if ($chapter > 1)
-		$u .= '?chapter=' . $i;
+		$u .= '?chapter=' . $chapter;
 	$fburl = 'http://graph.facebook.com/fql?q=SELECT%20url,%20normalized_url,%20share_count,%20like_count,%20comment_count,%20total_count,%20commentsbox_count,%20comments_fbid,%20click_count%20FROM%20link_stat%20WHERE%20url=%22'.$u.'%22';
 	$json = file_get_contents($fburl);
 	$fb = json_decode($json);
